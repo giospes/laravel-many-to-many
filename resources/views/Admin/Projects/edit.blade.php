7d3c3c16e1c75d2fa59b53c9f711142e7771df8a
giospes <giovannijacopo.spes@gmail.com>
2023-06-11 21:26:28 +0200

    some fixings

--- resources/views/Admin/Projects/edit.blade.php
+++ resources/views/Admin/Projects/edit.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-  
     <div class="container">
         <h1>Edit Project</h1>
         <form method="POST" action="{{ route('admin.projects.update', $project->slug) }}">
@@ -9,24 +8,24 @@
             @method('PUT')
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ $project->name }}" required>
+                <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $project->name) }}" required>
                 @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea name="description" id="description" class="form-control @error('description') is-invalid @enderror" required>{{ $project->description }}</textarea>
+                <textarea name="description" id="description" class="form-control @error('description') is-invalid @enderror" required>{{ old('description', $project->description) }}</textarea>
                 @error('description')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="mb-3">
-                <label for="type_id">type</label>
+                <label for="type_id">Type</label>
                 <select name="type_id" id="type_id" class="form-control @error('type_id') is-invalid @enderror">
-                    <option value="">Seleziona categoria</option>
+                    <option value="">Select a category</option>
                     @foreach ($types as $type)
-                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                        <option value="{{ $type->id }}" {{ old('type_id', $project->type_id) == $type->id ? 'selected' : '' }}>{{ $type->name }}</option>
                     @endforeach
                 </select>
                 @error('type_id')
@@ -37,7 +36,7 @@
                 <label for="technologies">Technologies:</label><br>
                 @foreach ($technologies as $technology)
                     <input type="checkbox" id="technologies" name="technologies[]" value="{{ $technology->id }}"
-                        @if ($project->technologies->contains($technology->id)) checked @endif>
+                        {{ in_array($technology->id, old('technologies', $project->technologies->pluck('id')->toArray())) ? 'checked' : '' }}>
                     <label for="technologies">{{ $technology->name }}</label><br>
                 @endforeach
             </div>
@@ -46,5 +45,5 @@
             </div>
         </form>
     </div>
-
 @endsection
+
